4f2406b72b7046129c58903c861ab0734308f8e2
andrejskvrn <andrejskvrn@gmail.com>
2023-04-19 22:46:57 +0200

    updated shipping

--- server/server.js
+++ server/server.js
@@ -40,43 +40,23 @@ app.post('/create-checkout-session', async (req, res) => {
     shipping_options: [
       {
         shipping_rate_data: {
-          type: "fixed_amount",
-          fixed_amount: {
-            amount: 0,
-            currency: "eur",
-          },
-          display_name: "Free shipping",
-          // Delivers between 5-7 business days
+          type: 'fixed_amount',
+          fixed_amount: {amount: 0, currency: 'usd'},
+          display_name: 'Free shipping',
           delivery_estimate: {
-            minimum: {
-              unit: "business_day",
-              value: 5,
-            },
-            maximum: {
-              unit: "business_day",
-              value: 7,
-            },
+            minimum: {unit: 'business_day', value: 5},
+            maximum: {unit: 'business_day', value: 7},
           },
         },
       },
       {
         shipping_rate_data: {
-          type: "fixed_amount",
-          fixed_amount: {
-            amount: 1500,
-            currency: "eur",
-          },
-          display_name: "Next day",
-          // Delivers in exactly 1 business day
+          type: 'fixed_amount',
+          fixed_amount: {amount: 1500, currency: 'usd'},
+          display_name: 'Next day air',
           delivery_estimate: {
-            minimum: {
-              unit: "business_day",
-              value: 1,
-            },
-            maximum: {
-              unit: "business_day",
-              value: 1,
-            },
+            minimum: {unit: 'business_day', value: 1},
+            maximum: {unit: 'business_day', value: 1},
           },
         },
       },
