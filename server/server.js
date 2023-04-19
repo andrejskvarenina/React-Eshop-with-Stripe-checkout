764b1e57568a6b92e8a065fc90beb1156d0c5b21
andrejskvrn <andrejskvrn@gmail.com>
2023-04-19 22:06:45 +0200

    added shipping

--- server/server.js
+++ server/server.js
@@ -33,8 +33,54 @@ app.post('/create-checkout-session', async (req, res) => {
   });
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
     line_items: lineItems,
+    shipping_address_collection: {
+      allowed_countries: ["SK", "CZ"],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 0,
+            currency: "eur",
+          },
+          display_name: "Free shipping",
+          // Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 5,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 1500,
+            currency: "eur",
+          },
+          display_name: "Next day",
+          // Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 1,
+            },
+          },
+        },
+      },
+    ],
     mode: 'payment',
     success_url: `${YOUR_DOMAIN}/success.html`,
     cancel_url: `${YOUR_DOMAIN}/cancel.html`,
